3a3bc45a7f8f241eeee01c94167dcf7490191415
Jake Faulkner <jakefaulkn@gmail.com>
2025-08-16 15:12:02 +1200

    oops should rotate the background

--- bedside/main.py
+++ bedside/main.py
@@ -57,7 +57,7 @@ async def process_event_loop(queue: Queue[Widget]) -> None:
 
 async def background(queue: Queue[Widget]) -> None:
     with resources.open_binary(bedside, "assets", "background.bmp") as f:
-        background_image = Image.open(f).convert(mode="1")
+        background_image = Image.open(f).convert(mode="1").rotate(90)
     red = Image.new("1", (WIDTH, HEIGHT), 255)
     widget = Widget(bw=background_image, red=red, name="background", z=-100)
     await queue.put(widget)
